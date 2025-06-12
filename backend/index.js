4823be4f2160c2ca93929ce50ba7f329b218ecbc
Shivam Kanojia <shivamkanojia.works@gmail.com>
2025-06-12 13:00:01 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
